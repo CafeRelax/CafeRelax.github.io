1abe2ddf2e36195256e67c11989eaf277f7c45b3
BogVseya <BogVseya@users.noreply.github.com>
2018-02-14 16:28:42 +0500

    18

---
+++ others/preloader.js
@@ -0,0 +1,8 @@
+document.body.onload=function(){
+ 	setTimeout(function(){
+      var preloader=document.getElementById("preloader");      
+         preloader.style.opacity=0; 
+         preloader.style.visibility="hidden"; 
+}
+		,800)
+}
